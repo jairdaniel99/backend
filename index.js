dfa7c2e29c5239216fc4a7998dd7a685540df8e7
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-03-25 16:43:07 -0600

    created server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(3000, () => {
+  console.log("The server is on");
+});
